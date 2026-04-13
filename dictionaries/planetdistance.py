d0be7c1df20c6c3571d03ad085c1205980102b5d
pabloT42 <saucysquirrel24@gmail.com>
2026-04-13 09:18:32 -0600

    Create planetdistance.py

---
+++ dictionaries/planetdistance.py
@@ -0,0 +1,9 @@
+planets = {}
+planets["mercury"] = 36
+planets["venus"] = 67
+planets["earth"] = 93
+planets["mars"] = 142
+for planet in planets.keys():
+    print(planet)
+for distance in planets.values():
+    print(distance)
